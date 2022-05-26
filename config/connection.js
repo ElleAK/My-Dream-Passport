46684fa3129ca539b824337c091a253bf95015ca
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-05-26 15:44:14 -0400

    file setup

---
+++ config/connection.js
@@ -0,0 +1,4 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS group-7-project_db;
+
+CREATE DATABASE group-7-project_db;

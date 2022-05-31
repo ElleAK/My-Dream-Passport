9c332f5da18eb6aae2d446d367f554328af3ffd2
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-05-31 18:57:46 -0400

    fixed model folder

--- model/index.js
+++
@@ -1,3 +0,0 @@
-const AddNew = require('./Add-new');
-const Dream = require('./Dream');
-const Visited = require('./Visited');
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {  } = require('../models');
 
 router.get('/', (req, res) => {
     res.render('homepage', {

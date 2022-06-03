c11bf234fff242003e79ce0fd077f097378a0269
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-06-02 20:24:39 -0400

    nothing

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { index, newDestination, visited } = require('../models');
+const {  } = require('../models');
 
 router.get('/', (req, res) => {
     res.render('homepage', {

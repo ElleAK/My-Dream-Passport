1a9cc0e4c327c155c428f6e6149b974f194bb0b0
Beepo <micahstew@gmail.com>
2022-06-06 16:22:26 -0400

    adding routes

---
+++ controllers/api/homepage-rout.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const newDestinationRoutes = require('./new-Destination-routes');
 const visitedRoutes = require('./visited-routes');
+const homepage = require('./homepage-rout');
+
+
 
 router.use('/visited', newDestinationRoutes);
 router.use('/newDestination', visitedRoutes);
-
+router.use('/homepage', homepage)
 
 module.exports = router;

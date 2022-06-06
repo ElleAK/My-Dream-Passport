711d35537a635521af8494e94d9c4270e057c146
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-06-06 19:29:46 -0400

    fixed users

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 //import all models
 const newDestination = require('./newDestination');
 const visited = require('./visited');
+const users = require('./users');
+
+//create associations
 
 
 
-module.exports = { newDestination, visited };
+module.exports = { newDestination, visited, users };
---
+++ models/users.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+// create User Model
+class users extends Model {
+    //method to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+    }
+
+// Columns for User model
+    users.init(
+        {
+            id: {
+              type: DataTypes.INTEGER,
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true
+            },
+            username: {
+              type: DataTypes.STRING,
+              allowNull: false
+            },
+            email: {
+              type: DataTypes.STRING,
+              allowNull: false,
+              unique: true,
+              validate: {
+                isEmail: true
+              }
+            },
+            password: {
+              type: DataTypes.STRING,
+              allowNull: false,
+              validate: {
+                len: [6]
+              }
+            }
+          },
+          {
+            hooks: {
+              // beforeCreate "hook" functionality
+              async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+              },
+        
+              async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+              }
+            },
+            sequelize,
+            timestamps: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'user'
+          }
+        );
+        
+        module.exports = users;
+        
